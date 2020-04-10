0fc48238d0b65a28c7569cfa73a3ae6208d52c67
Eduardo Mejia Bautista <ing.eduardo.mejia.bautista@gmail.com>
2020-04-10 17:28:35 -0500

    updateimages

--- app/src/main/java/com/example/fetchjson/MainActivity.kt
+++ app/src/main/java/com/example/fetchjson/MainActivity.kt
@@ -4,7 +4,10 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.gson.GsonBuilder
 import kotlinx.android.synthetic.main.activity_main.*
+import okhttp3.*
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,7 +16,42 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         recyclerView.layoutManager= LinearLayoutManager(this)
-        recyclerView.adapter= MainAdapter()
+        //recyclerView.adapter= MainAdapter()
+
+        fetchJson()
 
     }
+
+    private fun fetchJson() {
+        println("Attempting to Fetch JSON")
+        val url ="https://api.letsbuildthatapp.com/youtube/home_feed"
+        val request = Request.Builder().url(url).build()
+
+        val client= OkHttpClient()
+        client.newCall(request).enqueue(object :Callback{
+            override fun onResponse(call: Call, response: Response) {
+                val body = response?.body?.string()
+                println(body) //debug point para ver las variable
+
+                val gson = GsonBuilder().create()
+
+                val homeFeed= gson.fromJson(body,HomeFeed::class.java)
+                runOnUiThread {
+                    recyclerView.adapter=MainAdapter(homeFeed)
+                }
+            }
+
+            override fun onFailure(call: Call, e: IOException) {
+                println("Excecute request failed")
+            }
+
+        })
+
+        }
+
 }
+
+class HomeFeed(val videos:List<Video>)
+class Video(val id:Int,val name: String,val link:String,val imageUrl:String,
+            val numberOfViews:Int, val channel:Channel)
+class Channel(val name:String, val profileImageUrl:String)
--- app/src/main/java/com/example/fetchjson/MainAdapter.kt
+++ app/src/main/java/com/example/fetchjson/MainAdapter.kt
@@ -4,13 +4,14 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.video_row.view.*
 
-class MainAdapter : RecyclerView.Adapter<CustomViewHolder>(){
+class MainAdapter (val homeFeed: HomeFeed): RecyclerView.Adapter<CustomViewHolder>(){
     val videoTitles = listOf("First Title", "SecondTitle","3rd")
 
     override fun getItemCount(): Int {
-        return videoTitles.size
+        return homeFeed.videos.count()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
@@ -21,8 +22,13 @@ class MainAdapter : RecyclerView.Adapter<CustomViewHolder>(){
     }
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
-        val videoTitles=videoTitles.get(position)
-        holder?.view.textView_video_title?.text=videoTitles
+        //val videoTitles=videoTitles.get(position)
+        val video=homeFeed.videos.get(position)
+        holder?.view.textView_video_title?.text=video.name
+        holder?.view.textView_channel_name.text=video.channel.name
+
+        val thumbnailImageView = holder?.view?.imv_vid_thumbnail
+        Picasso.with(holder?.view.context).load(video.imageUrl).into(thumbnailImageView)
 
     }
 }
